64fc4d491602c9801e5f95b9d1e7c495c16b8d8b
Shane D Wilson <me@shanedwilson.com>
2018-09-01 12:14:16 -0500

    inclass debugging code

---
+++ main.js
@@ -0,0 +1,20 @@
+// const name = "Fred"
+
+// if (name === "Fred") {
+//    console.log("Hello Fred")
+// };
+
+// const drinkingAge = 21
+// const age = 35
+
+// if (age < drinkingAge) {
+//     console.log("You can't drink here")
+// } else {
+//     console.log("Have a beer")
+// };
+
+const colors = ["Red", "Orange", "Periwinkle", "Goldenrod", "Tomato", "Dodger Blue"]
+
+for (let i = 0; i < colors.length; i++) {
+    console.log(colors[i])
+}
